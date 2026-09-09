4dabac35fa06757a11247cfa0ba7eaec9d04e230
gowthamkanagaraj <gowthmkanagaraj@gmail.com>
2026-09-09 18:56:55 +0530

    Configure WhiteNoise for static file management
    Added WhiteNoise middleware for static file handling and configured static file storage.

--- payroll_project/settings.py
+++ payroll_project/settings.py
@@ -27,10 +27,9 @@ INSTALLED_APPS = [
     "payroll",
 ]
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware", "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware", "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware", "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
 ]
 ROOT_URLCONF = "payroll_project.urls"
 TEMPLATES = [{
@@ -61,6 +60,12 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/"
